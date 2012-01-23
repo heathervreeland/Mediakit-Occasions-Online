24e40db1069ebccd52342e6912b2e2c022c5964b
Web Developer <webmaster@occasionsonline.com>
2012-01-23 14:27:42 -0500

    changed local-print to local and national-print to national in submenu logic

--- header.php
+++ header.php
@@ -87,10 +87,10 @@ $walker = new Custom_Walker_Nav_Menu();
           if ( is_section_check( 'about-occasions' ) ) {
             wp_nav_menu( array( 'menu' => 'about', 'container_class' => 'menu-sub-header', 'after' => '<span class="pipe">|</span>', 'walker' => $walker ) ); 
             echo "<script>$('.menu-sub-header .menu').prepend('<li class=\'sub-menu-header\'>About Occasions</li>');</script>";
-          } elseif ( is_section_check( 'national-print' ) ) {
+          } elseif ( is_section_check( 'national' ) ) {
             wp_nav_menu( array( 'menu' => 'national-print', 'container_class' => 'menu-sub-header', 'after' => '<span class="pipe">|</span>', 'walker' => $walker ) ); 
             echo "<script>$('.menu-sub-header .menu').prepend('<li class=\'sub-menu-header\'>National Print</li>');</script>";
-          } elseif ( is_section_check( 'local-print' ) ) {
+          } elseif ( is_section_check( 'local' ) ) {
             wp_nav_menu( array( 'menu' => 'local-print', 'container_class' => 'menu-sub-header', 'after' => '<span class="pipe">|</span>', 'walker' => $walker ) ); 
             echo "<script>$('.menu-sub-header .menu').prepend('<li class=\'sub-menu-header\'>Local Print</li>');</script>";
           } elseif ( is_section_check( 'online' ) ) {
