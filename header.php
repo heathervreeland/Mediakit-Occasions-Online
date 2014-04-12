c5733c7bba6fa39d68038c4e15bb7b0c596be882
heathervreeland <webmaster@occasionsonline.com>
2014-04-12 15:20:30 -0400

    removed $page_classes removed custom walker updated javascript for secondary navigation headers updated logic testing to see if we are on a full width page

--- header.php
+++ header.php
@@ -8,20 +8,10 @@
  *
  */
 
-// determine best option for main navigation output
-// pull the existing menus to see if any have been created in the GUI
-$locations = get_nav_menu_locations();
-
-// assign the primary value to a variable for testing
-$this_location = $locations['primary'];
-
-// create a walker to be used if a menu has been created
-$walker = new Custom_Walker_Nav_Menu();
-
 // lets just be sure the current $post object is available for is_section_check() for left navigation below
 global $post;
 
-?><!DOCTYPE html  xmlns:fb="http://www.facebook.com/2008/fbml">
+?><!DOCTYPE html>
 <!--[if lt IE 7 ]> <html <?php language_attributes('html'); ?> class="no-js ie6"> <![endif]-->
 <!--[if IE 7 ]>    <html <?php language_attributes('html'); ?> class="no-js ie7"> <![endif]-->
 <!--[if IE 8 ]>    <html <?php language_attributes('html'); ?> class="no-js ie8"> <![endif]-->
@@ -64,8 +54,7 @@ global $post;
 
 </script>
 </head>
-<?php flush() ?>
-<body id="<?php echo $post->post_name; ?>" <?php body_class($page_class); ?>>
+<body id="<?php echo $post->post_name; ?>" <?php body_class(); ?>>
   <div id="wrapper" class="hfeed">
   
     <img src="<?php echo get_bloginfo('stylesheet_directory'); ?>/images/THE-MAGAZINE-FOR-CELEBRATING-IN-STYLE.png" width="356" height="60" alt="The Magazine for Celebrating in Style">
@@ -81,8 +70,8 @@ global $post;
       <a href="http://www.occasionsonline.com/register/"><img src="<?php echo get_bloginfo('stylesheet_directory'); ?>/images/icon-em.png" alt="" border="0" /></a>
       <a href="http://www.facebook.com/pages/Occasions-Magazine/145861875458364" target="_blank"><img src="<?php echo get_bloginfo('stylesheet_directory'); ?>/images/icon-fb.png" alt="" border="0" /></a>
       <a href="http://twitter.com/OccasionsMag" target="_blank"><img src="<?php echo get_bloginfo('stylesheet_directory'); ?>/images/icon-tw.png" alt="" border="0" /></a>
-       <a href="http://pinterest.com/OccasionsMag" target="_blank"><img src="<?php echo get_bloginfo('stylesheet_directory'); ?>/images/icon-pi.png" alt="" border="0" /></a>
-        <a href="https://plus.google.com/103715157874487467654" target="_blank"><img src="<?php echo get_bloginfo('stylesheet_directory'); ?>/images/icon-gp.png" alt="" border="0" /></a>
+      <a href="http://pinterest.com/OccasionsMag" target="_blank"><img src="<?php echo get_bloginfo('stylesheet_directory'); ?>/images/icon-pi.png" alt="" border="0" /></a>
+      <a href="https://plus.google.com/103715157874487467654" target="_blank"><img src="<?php echo get_bloginfo('stylesheet_directory'); ?>/images/icon-gp.png" alt="" border="0" /></a>
 
     </div>
 
@@ -95,15 +84,7 @@ global $post;
           <?php /*  Allow screen readers / text browsers to skip the navigation menu and get right to the good stuff */ ?>
           <div class="skip-link screen-reader-text"><a href="#content" title="<?php esc_attr_e( 'Skip to content', 'wfts' ); ?>"><?php _e( 'Skip to content', 'wfts' ); ?></a></div>
           <?php /* Our navigation menu.  If one isn't filled out, wp_nav_menu falls back to wp_page_menu.  The menu assiged to the primary position is the one used.  If none is assigned, the menu with the lowest ID is used.  */ ?>
-          <?php 
-          // ok, test to see if a menu has been assigned to the primary navigation and apply the custom walker, else apply the custom wfts_page_menu fallback function
-          if ( $this_location > 0 ) {
-          wp_nav_menu( array( 'container_class' => 'menu-header', 'walker' => $walker ) ); 
-          } else {
-          wp_nav_menu( array( 'container_class' => 'menu-header', 'fallback_cb' => 'wfts_page_menu' ) ); 
-          }
-
-          ?>
+          <?php wp_nav_menu( array( 'container_class' => 'menu-header') ); ?>
 
       </nav>
 
@@ -117,52 +98,56 @@ global $post;
         <?php echo insert_slider(); ?>
       </div>
       <?php } ?>
-      <?php if ( $template != 'full' ) { ?>
+      <?php if ( !is_page_template('page-full-width.php' ) ) { ?>
       <nav id="access-left">
         <?php
 
           // clunky but effective manner to determine which menu to pull
           // current $post object is passed to is_section_chec() for ancestry access
+          // header image/text is called via jquery
 
           if ( is_front_page() || is_home() ) {
-            wp_nav_menu( array('menu' => 'Quick Links', 'container_class' => 'menu-sub-header', 'walker' => $walker ));
-            echo "<script>$('.menu-sub-header .menu').prepend('<li class=\'sub-menu-header\'>Quick Links</li>');</script>";
+            wp_nav_menu( array('menu' => 'Quick Links', 'container_class' => 'menu-sub-header' ));
+            echo "<script>jQuery(document).ready( function($) { $('.menu-sub-header .menu').prepend('<li class=\'sub-menu-header\'>Quick Links</li>') });</script>";
           } elseif ( is_section_check( 'about-occasions', $post ) ) {
-            wp_nav_menu( array( 'menu' => 'about', 'container_class' => 'menu-sub-header', 'walker' => $walker ) ); 
-            echo "<script>$('.menu-sub-header .menu').prepend('<li class=\'sub-menu-header\'>About Occasions</li>');</script>";
+            wp_nav_menu( array( 'menu' => 'about', 'container_class' => 'menu-sub-header' ) ); 
+            echo "<script>jQuery(document).ready( function($) { $('.menu-sub-header .menu').prepend('<li class=\'sub-menu-header\'>About Occasions</li>') });</script>";
           } elseif ( is_section_check( 'national', $post ) ) {
-            wp_nav_menu( array( 'menu' => 'national-print', 'container_class' => 'menu-sub-header', 'walker' => $walker ) ); 
-            echo "<script>$('.menu-sub-header .menu').prepend('<li class=\'sub-menu-header\'>National</li>');</script>";
+            wp_nav_menu( array( 'menu' => 'national-print', 'container_class' => 'menu-sub-header' ) ); 
+            echo "<script>jQuery(document).ready( function($) { $('.menu-sub-header .menu').prepend('<li class=\'sub-menu-header\'>National</li>') });</script>";
           } elseif ( is_section_check( 'local', $post ) ) {
-            wp_nav_menu( array( 'menu' => 'local-editions', 'container_class' => 'menu-sub-header', 'walker' => $walker ) ); 
-            echo "<script>$('.menu-sub-header .menu').prepend('<li class=\'sub-menu-header\'>Local Editions</li>');</script>";
+            wp_nav_menu( array( 'menu' => 'local-editions', 'container_class' => 'menu-sub-header' ) ); 
+            echo "<script>jQuery(document).ready( function($) { $('.menu-sub-header .menu').prepend('<li class=\'sub-menu-header\'>Local Editions</li>') });</script>";
           } elseif ( is_section_check( 'destination-occasions', $post ) ) {
-            wp_nav_menu( array( 'menu' => 'destination-occasions', 'container_class' => 'menu-sub-header', 'walker' => $walker ) ); 
-            echo "<script>$('.menu-sub-header .menu').prepend('<li class=\'sub-menu-header\'>Destination</li>');</script>";
+            wp_nav_menu( array( 'menu' => 'destination-occasions', 'container_class' => 'menu-sub-header' ) ); 
+            echo "<script>jQuery(document).ready( function($) { $('.menu-sub-header .menu').prepend('<li class=\'sub-menu-header\'>Destination</li>') });</script>";
           } elseif ( is_section_check( 'online', $post ) ) {
-            wp_nav_menu( array( 'menu' => 'online', 'container_class' => 'menu-sub-header', 'walker' => $walker ) ); 
-            echo "<script>$('.menu-sub-header .menu').prepend('<li class=\'sub-menu-header\'>Online</li>');</script>";
+            wp_nav_menu( array( 'menu' => 'online', 'container_class' => 'menu-sub-header' ) ); 
+            echo "<script>jQuery(document).ready( function($) { $('.menu-sub-header .menu').prepend('<li class=\'sub-menu-header\'>Online</li>') });</script>";
           } elseif ( is_section_check( 'print-advertising', $post ) ) {
-            wp_nav_menu( array( 'menu' => 'print-advertising', 'container_class' => 'menu-sub-header', 'walker' => $walker ) ); 
-            echo "<script>$('.menu-sub-header .menu').prepend('<li class=\'sub-menu-header\'>Print</li>');</script>";
+            wp_nav_menu( array( 'menu' => 'print-advertising', 'container_class' => 'menu-sub-header' ) ); 
+            echo "<script>jQuery(document).ready( function($) { $('.menu-sub-header .menu').prepend('<li class=\'sub-menu-header\'>Print</li>') });</script>";
           } elseif ( is_section_check( 'online-advertising', $post ) ) {
-            wp_nav_menu( array( 'menu' => 'online', 'container_class' => 'menu-sub-header', 'walker' => $walker ) ); 
-            echo "<script>$('.menu-sub-header .menu').prepend('<li class=\'sub-menu-header\'>Online Advertising</li>');</script>";
+            wp_nav_menu( array( 'menu' => 'online', 'container_class' => 'menu-sub-header' ) ); 
+            echo "<script>jQuery(document).ready( function($) { $('.menu-sub-header .menu').prepend('<li class=\'sub-menu-header\'>Online Advertising</li>') });</script>";
           } elseif ( is_section_check( 'networking', $post ) ) {
-            wp_nav_menu( array( 'menu' => 'networking-events', 'container_class' => 'menu-sub-header', 'walker' => $walker ) ); 
-            echo "<script>$('.menu-sub-header .menu').prepend('<li class=\'sub-menu-header\'>Networking Events</li>');</script>";
+            wp_nav_menu( array( 'menu' => 'networking-events', 'container_class' => 'menu-sub-header' ) ); 
+            echo "<script>jQuery(document).ready( function($) { $('.menu-sub-header .menu').prepend('<li class=\'sub-menu-header\'>Networking Events</li>') });</script>";
           } elseif ( is_section_check( 'expo', $post ) ) {
-            wp_nav_menu( array( 'menu' => 'expo', 'container_class' => 'menu-sub-header', 'walker' => $walker ) ); 
-            echo "<script>$('.menu-sub-header .menu').prepend('<li class=\'sub-menu-header\'>Expo</li>');</script>";
+            wp_nav_menu( array( 'menu' => 'expo', 'container_class' => 'menu-sub-header' ) ); 
+            echo "<script>jQuery(document).ready( function($) { $('.menu-sub-header .menu').prepend('<li class=\'sub-menu-header\'>Expo</li>') });</script>";
           } elseif ( is_section_check( 'events', $post ) ) {
-            wp_nav_menu( array( 'menu' => 'events', 'container_class' => 'menu-sub-header', 'walker' => $walker ) ); 
-            echo "<script>$('.menu-sub-header .menu').prepend('<li class=\'sub-menu-header\'>Events</li>');</script>";
+            wp_nav_menu( array( 'menu' => 'events', 'container_class' => 'menu-sub-header' ) ); 
+            echo "<script>jQuery(document).ready( function($) { $('.menu-sub-header .menu').prepend('<li class=\'sub-menu-header\'>Events</li>') });</script>";
           } elseif ( $post->post_name == 'our-reach' ) {
-            wp_nav_menu( array( 'menu' => 'reach', 'container_class' => 'menu-sub-header', 'walker' => $walker ) ); 
-            echo "<script>$('.menu-sub-header .menu').prepend('<li class=\'sub-menu-header\'>Our Reach</li>');</script>";
+            wp_nav_menu( array( 'menu' => 'reach', 'container_class' => 'menu-sub-header' ) ); 
+            echo "<script>jQuery(document).ready( function($) { $('.menu-sub-header .menu').prepend('<li class=\'sub-menu-header\'>Our Reach</li>') });</script>";
           } elseif ( is_section_check( 'marketing-program', $post ) ) {
-            wp_nav_menu( array( 'menu' => 'marketing-programs', 'container_class' => 'menu-sub-header', 'walker' => $walker ) ); 
-            echo "<script>$('.menu-sub-header .menu').prepend('<li class=\'sub-menu-header\'>Membership</li>');</script>";
+            wp_nav_menu( array( 'menu' => 'marketing-programs', 'container_class' => 'menu-sub-header' ) ); 
+            echo "<script>jQuery(document).ready( function($) { $('.menu-sub-header .menu').prepend('<li class=\'sub-menu-header\'>Membership</li>') });</script>";
+          } elseif ( is_section_check( 'member-resources', $post ) ) {
+            wp_nav_menu( array( 'menu' => 'member-resources', 'container_class' => 'menu-sub-header' ) ); 
+            echo "<script>jQuery(document).ready( function($) { $('.menu-sub-header .menu').prepend('<li class=\'sub-menu-header\'>Membership Resources</li>') });</script>";
           }
 
         ?>
