7d5eff09ee8d661a9d178bd4d53977233d555357
heathervreeland <webmaster@occasionsonline.com>
2014-04-12 15:18:27 -0400

    removed unused files

--- inc/help-media.php
+++
@@ -1,8 +0,0 @@
-<p>All the files you&#8217;ve uploaded are listed in the Media Library, with the most recent uploads listed first. You can use the <em>Screen Options</em> tab to customize the display of this screen.</p>
-<p>You can narrow the list by file type/status using the text link filters at the top of the screen. You also can refine the list by date using the dropdown menu above the media table.</p>
-<p>Hovering over a row reveals action links: <em>Edit</em>, <em>Delete Permanently</em>, and <em>View</em>. Clicking <em>Edit</em> or on the media file&#8217;s name displays a simple screen to edit that individual file&#8217;s metadata. Clicking <em>Delete Permanently</em> will delete the file from the media library (as well as from any posts to which it is currently attached). <em>View</em> will take you to the display page for that file.</p>
-<p>If a media file has not been attached to any post, you will see that in the <em>Attached To</em> column, and can click on <em>Attach File</em> to launch a small popup that will allow you to search for a post and attach the file.</p>
-<p><strong>For more information:</strong></p>
-<p><a href="http://codex.wordpress.org/Media_Library_SubPanel" target="_blank">Media Library Documentation</a></p>
-<p><a href="http://wordpress.org/support/" target="_blank">Support Forums</a></p>
-View a video on how to <a href="<?php bloginfo(stylesheet_directory); ?>/inc/video-insert-file.php?TB_iframe=true&height=600&width=700&modal=true" title="How to Insert a PDF or Image file into a Page" class="thickbox">insert a PDF or Image file into a page</a>.
